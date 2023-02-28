e9b16848b0d01fd0b63d9951b2433263098b7206
fuwamaki <variety.fuwa.3@gmail.com>
2023-02-28 15:58:38 +0900

    rename for camelCase

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./assets/css/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import ResponsiveDrawer from "./responsive_drawer";
+import ResponsiveDrawer from "./responsiveDrawer";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     {/* <App /> */}
