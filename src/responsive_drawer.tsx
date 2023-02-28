fde485e40bbe034d8f95dff20495e980256d6191
fuwamaki <variety.fuwa.3@gmail.com>
2023-02-28 14:49:37 +0900

    add graph on responsive_drawer

--- src/responsive_drawer.tsx
+++ src/responsive_drawer.tsx
@@ -19,6 +19,7 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
+import Demo from "./demo";
 
 const drawerWidth = 240;
 
@@ -178,6 +179,7 @@ export default function ResponsiveDrawer(props: Props) {
           <CardActions>
             <Button size="small">Learn More</Button>
           </CardActions>
+          <Demo />
         </Card>
       </Box>
     </Box>
