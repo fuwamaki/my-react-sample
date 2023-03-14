6b973aea42357540debab220cf1d00b73abb6b27
fuwamaki <variety.fuwa.3@gmail.com>
2023-03-14 09:41:29 +0900

    set font-family

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "App";
 import reportWebVitals from "reportWebVitals";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "Helvetica Neue",
+      "Helvetica",
+      "Arial",
+      "Meiryo UI",
+      "メイリオ",
+      "Meiryo",
+      "ＭＳ Ｐゴシック",
+      "MS PGothic",
+    ].join(","),
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
