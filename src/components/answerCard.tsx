497ee4d2a789f0999bad1349a5a174d11157337d
fuwamaki <variety.fuwa.3@gmail.com>
2023-04-18 11:55:49 +0900

    fix answerCard

--- src/components/answerCard.tsx
+++ src/components/answerCard.tsx
@@ -1,49 +1,78 @@
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartOptions,
+} from "chart.js";
 import { Bar } from "react-chartjs-2";
 import { faker } from "@faker-js/faker";
+import { Typography, useTheme } from "@mui/material";
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export const options = {
+  indexAxis: "y" as const,
   responsive: true,
   plugins: {
     legend: {
-      position: "top" as const,
+      display: false,
     },
     title: {
-      display: true,
-      text: "Chart.js Bar Chart",
+      display: false,
     },
   },
+  scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+      ticks: {
+        color: "red",
+        showLabelBackdrop: true,
+      },
+    },
+    y: {
+      ticks: {
+        color: "red",
+      },
+    },
+  },
+  //   scales: {
+  //     x: {
+  //
+  //     },
+  //   },
 };
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Dataset 2",
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ],
-};
+const labels = ["出社", "在宅", "変わらない", "比べた経験がない"];
 
 export default function AnswerCard() {
+  const theme = useTheme();
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+        backgroundColor: theme.palette.primary.main,
+      },
+    ],
+  };
   return (
     <Box sx={{ maxWidth: 300 }}>
       <Card variant="outlined">
         <CardContent>
-          <Bar options={options} data={data} />;
+          <Typography variant="body2" sx={{ mb: 2 }}>
+            仕事は出社と在宅、どちらの方が集中できる？
+          </Typography>
+          <Bar options={options} data={data} />
         </CardContent>
       </Card>
     </Box>
