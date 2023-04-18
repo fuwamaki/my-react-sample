715e98405a9b236def0d19df426acbc23cd0af43
fuwamaki <variety.fuwa.3@gmail.com>
2023-04-18 10:29:16 +0900

    add Stack on trendPage

--- src/pages/trendPage.tsx
+++ src/pages/trendPage.tsx
@@ -1,10 +1,14 @@
+import { Stack } from "@mui/material";
 import QuestionCard from "components/questionCard";
 
 export default function TrendPage() {
   return (
     <div>
       <h2>Trend</h2>
-      <QuestionCard />
+      <Stack direction="row" spacing={2}>
+        <QuestionCard />
+        <QuestionCard />
+      </Stack>
     </div>
   );
 }
